343b99d700681dfdc083a884eed63e4baf3bd02d
Maycon Looma_Code <mayconkluser@loomacode.com.br>
2022-05-04 00:39:53 -0300

    add js em perguntas freguentes e parâmetros URL

--- projeto-bikcraft/js/script.js
+++ projeto-bikcraft/js/script.js
@@ -1,12 +1,40 @@
+// ATIVAR LINKS DO MENU
 const links = document.querySelectorAll(".header-menu a");
-
 function ativaLink(link) {
   const url = window.location.href;
   const href = link.href;
-
   if (url.includes(href)) {
     link.classList.add("ativo");
   }
 }
-
 links.forEach(ativaLink);
+
+// ATIVAR ITENS DO ORÇAMENTO
+const parametros = new URLSearchParams(location.search);
+function ativaProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativaProduto);
+
+// PERGUNTAS FREQUENTES
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
+
+console.log(perguntas);
